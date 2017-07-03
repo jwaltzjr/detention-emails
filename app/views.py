916cf78d263410ce3d7b4404d0a174b48aa8b27f
jwaltzjr <jwaltzjr@hotmail.com>
2017-07-03 12:09:31 -0500

    Set up a testing view with all of the db relationships

--- app/views.py
+++ app/views.py
@@ -1,7 +1,28 @@
+from collections import defaultdict
+from flask import render_template
+
 from app import app
 from . import models
 
-@app.route('/')
-def index():
-    fb = models.Tlorder.query.first()
-    return fb.bill_number
+@app.route('/test')
+def test():
+    trip = models.Trip.query.filter_by(trip_number=10321).one()
+    freight_bills = trip.termplans.filter_by(
+        trip_number=trip.trip_number
+    )
+    
+    stops = defaultdict(list)
+    for fb in freight_bills:
+        if fb.tx_type == 'P':
+            group = 'P-{}-{}'.format(fb.tlorder.origin, fb.tlorder.pick_up_by)
+            stops[group].append(fb.tlorder)
+        elif fb.tx_type == 'D':
+            group = 'D-{}-{}'.format(fb.tlorder.destination, fb.tlorder.deliver_by)
+            stops[group].append(fb.tlorder)
+            
+    return render_template(
+        'testing.html',
+        trip = trip,
+        freight_bills = freight_bills,
+        stops = stops.items()
+    )
