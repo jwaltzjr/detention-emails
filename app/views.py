b0a4ae0760bbcaca4113b70b452e9cf000b0176c
jwaltzjr <jwaltzjr@hotmail.com>
2017-07-06 15:50:20 -0500

    Added send_email functionality, but getting error about JSON serialization

--- app/views.py
+++ app/views.py
@@ -1,6 +1,7 @@
 from collections import defaultdict
-from flask import render_template, redirect
+from flask import render_template, redirect, request, session
 
+from krc.krcemail import KrcEmail
 from app import app
 from . import models, forms
 
@@ -44,6 +45,8 @@ def index():
             elif fb.tx_type == 'D':
                 group = 'D-{}-{}'.format(fb.tlorder.destination, fb.tlorder.deliver_by)
                 stops[group].append(fb.tlorder)
+
+        session['trip_stops'] = stops
                 
         return render_template(
             'index.html',
@@ -55,4 +58,47 @@ def index():
         'index.html',
         form=form
     )
+
+@app.route('/send-email', methods=['GET','POST'])
+def send_email():
+    stops = session.pop('trip_stops', None)
+    stop_id = request.args.get('stop')
+    if stops and stop_id:
+        stop_type = stop_id[0]
+        freight_bills = stops[stop_id]
+        
+        email_message_base = (
+            "Hello,\n\nThis is an automated message from KRC Logistics. We are currently at {location} for "
+            "our scheduled appointment at {appointment} and our driver is being detained. Depending on when "
+            "the driver is released, detention charges may apply on the following order.\n\nThank you and "
+            "have a great day!\n\n{fb_info}"
+        )
+        test_message = ''
+        
+        for fb in freight_bills:
+            bol_list = ','.join(fb.trace_numbers.filter_by(trace_type='B').all())
+            po_list = ','.join(fb.trace_numbers.filter_by(trace_type='P').all())
+            fb_info = ("FB# {fb}\nBOL#(s) {bols}\nPO#(s) {pos}\n").format(
+                fb=fb.bill_number,
+                bols=bol_list,
+                pos=po_list
+            )
+            if stop_type == 'P':
+                email_message = email_message_base.format(
+                    location=fb.origname,
+                    appointment=fb.pick_up_by,
+                    fb_info=fb_info
+                )
+            elif stop_type == 'D':
+                email_message = email_message_base.format(
+                    location=fb.destname,
+                    appointment=fb.deliver_by,
+                    fb_info=fb_info
+                )
+            else:
+                email_message = 'An error occured.\n\n{}'.format(fb_info)
+            test_message += (email_message + '\n\n')
+        return test_message
+    else:
+        return 'An error occured.'
 
