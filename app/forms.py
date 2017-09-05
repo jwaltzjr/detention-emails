9d8b9c2dd426b75057e645840b5a70f183065e72
jwaltzjr <jwaltzjr@hotmail.com>
2017-09-05 15:36:49 -0500

    Add undelivered form

--- app/forms.py
+++ app/forms.py
@@ -1,9 +1,17 @@
 from flask_wtf import FlaskForm
-from wtforms import IntegerField
+from wtforms import IntegerField, SelectMultipleField, widgets
 from wtforms.validators import DataRequired
 
 class TripNumberForm(FlaskForm):
     trip_no = IntegerField(
         'trip_no',
         validators=[DataRequired()]
+    )
+
+class UndeliveredForm(FlaskForm):
+    freight_bills = SelectMultipleField(
+        'Which freight bills are undelivered?',
+        choices=[],
+        option_widget=widgets.CheckboxInput(),
+        widget=widgets.ListWidget(prefix_label=False)
     )
